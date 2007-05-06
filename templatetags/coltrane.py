1c71b802675558b07a64132bc9e36fb96c716f09
ubernostrum <ubernostrum@5f8205a5-902a-0410-8b63-8f478ce83d95>
2007-05-06 06:12:32 +0000

    Beginnings of templatetag refactor: tag for fetching latest featured content
    git-svn-id: http://coltrane-blog.googlecode.com/svn/trunk@30 5f8205a5-902a-0410-8b63-8f478ce83d95

--- templatetags/coltrane.py
+++ templatetags/coltrane.py
@@ -7,74 +7,34 @@ from coltrane.models import Entry, Link
 register = template.Library()
 
 
-class LatestContentNode(template.Node):
-    def __init__(self, queryset, num, varname):
-        self.queryset = queryset
-        self.num, self.varname = num, varname
+class LatestFeaturedNode(template.Node):
+    def __init__(self, varname):
+        self.varname = varname
     
     def render(self, context):
-        context[self.varname] = self.queryset[:self.num]
+        context[self.varname] = Entry.live.latest_featured()
         return ''
 
 
-def do_latest_entries(parser, token):
+def do_latest_featured(parser, token):
     """
-    Inserts the latest ``num`` published entries (i.e., those with
-    "live" status) into ``varname``.
+    Retrieves the latest featured Entry and stores it in a specified
+    context variable.
+    
+    Syntax::
+    
+        {% get_latest_featured_entry as [varname] %}
     
     Example::
     
-        {% get_latest_entries 5 as latest_entries %}
-        
-    """
-    bits = token.contents.split()
-    if len(bits) != 4:
-        raise template.TemplateSyntaxError("'%s' tag takes exactly three arguments" % bits[0])
-    return LatestContentNode(queryset=Entry.objects.live(),
-                             num=int(bits[1]),
-                             varname=bits[3])
-
-def do_latest_links(parser, token):
-    """
-    Inserts the latest ``num`` links into ``varname``.
-    
-    Example::
-    
-        {% get_latest_links 5 as latest_links %}
+        {% get_latest_featured_entry as latest_featured_entry %}
     
     """
     bits = token.contents.split()
-    if len(bits) != 4:
-        raise template.TemplateSyntaxError("'%s' tag takes exactly three arguments" % bits[0])
-    return LatestContentNode(queryset=Link.objects.all(),
-                             num=int(bits[1]),
-                             varname=bits[3])
+    if len(bits) != 3:
+        raise template.TemplateSyntaxError("'%s' tag takes two arguments" % bits[0])
+    if bits[1] != 'as':
+        raise template.TemplateSyntaxError("first argument to '%s' tag must be 'as'" % bits[0])
+    return LatestFeaturedNode(bits[2])
 
-def do_latest_comments(parser, token):
-    """
-    Inserts the latest ``num`` approved comments (i.e., those with
-    ``is_public=True`` into ``varname``.
-    
-    Pass ``FreeComment`` as the second argument if you're using the
-    ``FreeComment`` model, or ``Comment`` if you're using the ``Comment``
-    model.
-    
-    Example::
-    
-        {% get_latest_comments FreeComment 5 as latest_comments %}
-    
-    """
-    bits = token.contents.split()
-    if len(bits) != 5:
-        raise template.TemplateSyntaxError("'%s' tag takes exactly four arguments" % bits[0])
-    if bits[1] not in ("Comment", "FreeComment"):
-        raise template.TemplateSyntaxerror("First argument to '%s' tag must be the name of the comment model to use" % bits[0])
-    comment_model = { 'FreeComment': FreeComment,
-                      'Comment': Comment }[bits[1]]
-    return LatestContentNode(queryset=comment_model.objects.filter(is_public__exact=True),
-                                                                   num=int(bits[2]),
-                                                                   varname=bits[3])
-
-register.tag('get_latest_entries', do_latest_entries)
-register.tag('get_latest_links', do_latest_links)
-register.tag('get_latest_comments', do_latest_comments)
+register.tag('get_latest_featured_entry', do_latest_featured)
